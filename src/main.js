ed4bf4dd84c49eca4084bdd17f3642e2d961961a
Slađan Ristić <sladiri@users.noreply.github.com>
2017-01-30 21:59:13 +0100

    Add allowed actions. Move state-rep function.

--- src/main.js
+++ src/main.js
@@ -4,57 +4,84 @@ import h from 'inferno-hyperscript'
 import uuid from 'uuid/v4'
 
 const model = {
-  lastActionID: undefined,
-  actionID: undefined,
   value: undefined,
+  error: undefined,
 }
 
 const propose = model => input => {
+  model.error = null
+
   console.log('model', input)
-  model.lastActionID = input.actionID
-  model.actionID = uuid()
+
   model.value = input.value || model.value
   model.value += input.increment || 0
+
+
   state(model)
 }
 
 const actions = {
-  setValue (value) {
-    propose(model)({value, actionID: model.actionID})
+  setValue (allowedActions, value) {
+    if (!allowedActions.includes('setValue')) {
+      console.warn('setValue not allowed', allowedActions)
+      return
+    }
+
+    setTimeout(() => {
+      propose(model)({value})
+    }, 2000)
   },
-  increment () {
-    propose(model)({increment: 1, actionID: model.actionID})
+  increment (allowedActions) {
+    if (!allowedActions.includes('increment')) {
+      console.warn('increment not allowed', allowedActions)
+      return
+    }
+
+    propose(model)({increment: 1})
   },
 }
 
 const stateRepresentation = vm => {
+  const allowedActions = Object.keys(actions)
+    .filter(action => action === 'setValue' || vm.value !== undefined)
+
   return h('div', [
     h('h1', `Hey ${vm.value}`),
-    h('p', `Last actionID: [${vm.lastActionID}]`),
-    h('p', `ActionID: [${vm.actionID}]`),
+    vm.lastActionID ? h('p', `Last actionID: [${vm.lastActionID.substring(0, 7)}]`) : undefined,
+    vm.actionID ? h('p', `ActionID: [${vm.actionID.substring(0, 7)}]`) : undefined,
     h('button', {
       onclick (event) {
-        actions.setValue(vm.value + 1)
+        actions.setValue(allowedActions, vm.value + 1)
       },
     }, 'Set Value'),
     h('button', {
       onclick (event) {
-        actions.increment()
+        actions.increment(allowedActions)
       },
     }, 'Increment'),
+    h('br'),
+    h('input', {
+      placeholder: 'enter here',
+    }),
+    vm.error ? h('div', [
+      h('br'),
+      h('p', vm.error),
+    ]) : undefined,
   ])
 }
 
 const nap = model => {
-  if (model.value === undefined) { actions.setValue(1) }
+  if (model.value === undefined) { actions.setValue(['setValue'], 1) }
 }
 
-const state = juxt([
-  pipe(
-    stateRepresentation,
-    curry(render)(__, document.getElementById('root')),
-    ),
-  nap,
-])
+const state = model => {
+  return juxt([
+    pipe(
+      stateRepresentation,
+      curry(render)(__, document.getElementById('root')),
+      ),
+    nap,
+  ])(model)
+}
 
 state(model)
