3e5bee3b27f8b221f8fcc676bcc9a0357f3947b7
Slađan Ristić <sladiri@users.noreply.github.com>
2017-02-10 00:02:00 +0100

    Add simple example

---
+++ src/fab.js
@@ -0,0 +1,109 @@
+import Promise from 'bluebird'
+
+const root = document.getElementById('root-fab')
+const store = {
+  dispatch (data) {
+    document.dispatchEvent(new CustomEvent('redux', { detail: data })) // eslint-disable-line no-undef
+  },
+  getState () {
+    return this.state
+  },
+  state: {
+    config: null,
+    cms: null,
+  },
+}
+
+async function* samLoop ({
+  actionFac = (present) => ({}),
+  state = {},
+  presentFac = (state) => (model, reduxMessage) => { },
+  target = null,
+  napFac = (state, actions) => (model) => { },
+  view = (model) => { },
+  displayFac = (state, view) => (model) => { },
+}) {
+  // const actions = actionFac((data) => { store.dispatch({ type: 'PRESENT', payload: data }) })
+  const actions = actionFac()
+  const nap = napFac(state, actions)
+  const display = displayFac(state, view)
+
+  const model = store.getState()
+  document.addEventListener('reduxDone', function once () {
+    console.log('re-render caused by redux statepath')
+    root.innerHTML = display(model)
+    document.removeEventListener('reduxDone', once)
+  })
+  store.dispatch({ type: 'PRESENT' })
+  while (true) {
+    // await new Promise((resolve, reject) => { setTimeout(() => { resolve() }, 100) })
+
+    // const model = store.getState()
+
+    // console.log('render wanted')
+    // root.innerHTML = display(model)
+
+    const [action, data] = nap(model) || []
+    let proposal
+    if (action && actions[action]) {
+      proposal = await Promise.resolve(actions[action](data))
+    } else {
+      if (action) { console.warn('invalid action', action, data) }
+      console.log('waiting for input')
+      yield
+    }
+    await new Promise((resolve, reject) => {
+      document.addEventListener('reduxDone', ({ detail: model }) => {
+        console.log('re-render caused by redux statepath', model)
+        root.innerHTML = display(model)
+        resolve()
+      })
+      store.dispatch({ type: 'PRESENT', payload: proposal })
+    })
+  }
+}
+
+const actionFac = (present) => ({
+  // init (config) { present({ config }) },
+  // loadCms () { present({ cms: 'foo' }) },
+  init (config) { return { config } },
+  loadCms () { return { cms: 'foo' } },
+})
+
+const state = {
+  initial (model) { return !model.config },
+  configLoaded (model) { return model.config === 'confoo' },
+  cmsLoaded (model) { return model.cms === 'foo' },
+}
+
+const presentFac = (state) => function present (model, { type, payload }) {
+  console.log('present', model, payload)
+  if (payload) { // Check, since Redux is fake.
+    if (state.initial(model) && payload.config) { model.config = payload.config }
+    if (!state.cmsLoaded(model) && payload.cms) { model.cms = payload.cms }
+  }
+  document.dispatchEvent(new CustomEvent('reduxDone', { detail: model })) // eslint-disable-line no-undef
+  // return model // Only in real Redux.
+}
+document.addEventListener('redux', ({ detail }) => { // Fake redux store.
+  presentFac(state)(store.state, detail)
+})
+
+const napFac = (state, actions) => function nap (model) {
+  // if (state.initial(model)) { actions.init('confoo') }
+  if (state.initial(model)) { return ['init', 'confoo'] }
+}
+
+const view = {
+  initial (model) { return 'initial view' },
+  configReady (model) { return 'config ready view' },
+  cmsReady (model) { return 'CMS ready view' },
+}
+
+const displayFac = (state, view) => (model) => {
+  if (state.initial(model)) { return view.initial(model) }
+  if (state.configLoaded(model)) { return view.configReady(model) }
+  if (state.cmsLoaded(model)) { return view.cmsReady(model) }
+}
+const sl = samLoop({actionFac, state, presentFac, napFac, view, displayFac})
+sl.next()
--- src/main.js
+++ src/main.js
@@ -1,160 +1,162 @@
-import parentFactory, {actions as parentActions} from './parent'
+import './fab'
 
-const parentInstance = parentFactory((parentMmodel, parentState) => {
-  model.parent = parentState.name
-  const state = stateFn(model)
-  stateRepresentation({ vm: model, state })
-})
+// // import parentFactory, {actions as parentActions} from './parent'
 
-import sam from './fabric'
-import { render } from 'inferno'
-import h from 'inferno-hyperscript'
+// // const parentInstance = parentFactory((parentMmodel, parentState) => {
+// //   model.parent = parentState.name
+// //   const state = stateFn(model)
+// //   stateRepresentation({ vm: model, state })
+// // })
 
-const actions = {
-  cancelSetValue (input) {
-    return {}
-  },
-  setValue (input) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => { resolve({ value: input }) }, 3000)
-    })
-  },
-  increment (input) {
-    return { increment: input }
-  },
-  decrement (input) {
-    return { increment: input * (-1) }
-  },
-}
+// import sam from './fabric'
+// import { render } from 'inferno'
+// import h from 'inferno-hyperscript'
 
-function stateRepresentation ({vm, state: {name, allowedActions}}) {
-  const view = h('div', [
-    h('h1#hey', `Hey child ${vm.value}`),
-    h('p', vm.state),
-    h('p', `Parent's state: ${vm.parent}`),
-    h('p', [
-      h('button', {
-        onclick (event) {
-          instance({action: 'cancelSetValue', cancel: true})
-        },
-        disabled: !allowedActions.includes('cancelSetValue'),
-      }, 'Cancel'),
-      h('button', {
-        onclick (event) {
-          instance({action: 'setValue', input: vm.value + 1})
-        },
-        disabled: !allowedActions.includes('setValue'),
-      }, `Async: Set Value to ${vm.value + 1}`),
-    ]),
-    h('p', [
-      h('button', {
-        onclick (event) {
-          instance({action: 'increment', input: 1})
-        },
-        disabled: !allowedActions.includes('increment'),
-      }, 'Increment'),
-      h('button', {
-        onclick (event) {
-          instance({action: 'decrement', input: 1})
-        },
-        disabled: !allowedActions.includes('decrement'),
-      }, 'Decrement'),
-    ]),
-    h('p', [
-      h('input#inpu', {
-        placeholder: 'enter here',
-        style: { position: 'relative' },
-      }),
-    ]),
-    h('p', [
-      vm.error
-        ? h('div', [
-          h('br'),
-          h('p', vm.error),
-        ])
-        : undefined,
-    ]),
-  ])
-  render(view, document.getElementById('root-child'))
-}
+// const actions = {
+//   cancelSetValue (input) {
+//     return {}
+//   },
+//   setValue (input) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => { resolve({ value: input }) }, 3000)
+//     })
+//   },
+//   increment (input) {
+//     return { increment: input }
+//   },
+//   decrement (input) {
+//     return { increment: input * (-1) }
+//   },
+// }
 
-const model = {
-  // items: [
-  //   { id: 0, name: 'foo' },
-  //   { id: 1, name: 'bar' },
-  //   { id: 2, name: 'baz' },
-  // ],
-  value: undefined,
-  error: undefined,
-  parent: undefined,
-}
+// function stateRepresentation ({vm, state: {name, allowedActions}}) {
+//   const view = h('div', [
+//     h('h1#hey', `Hey child ${vm.value}`),
+//     h('p', vm.state),
+//     h('p', `Parent's state: ${vm.parent}`),
+//     h('p', [
+//       h('button', {
+//         onclick (event) {
+//           instance({action: 'cancelSetValue', cancel: true})
+//         },
+//         disabled: !allowedActions.includes('cancelSetValue'),
+//       }, 'Cancel'),
+//       h('button', {
+//         onclick (event) {
+//           instance({action: 'setValue', input: vm.value + 1})
+//         },
+//         disabled: !allowedActions.includes('setValue'),
+//       }, `Async: Set Value to ${vm.value + 1}`),
+//     ]),
+//     h('p', [
+//       h('button', {
+//         onclick (event) {
+//           instance({action: 'increment', input: 1})
+//         },
+//         disabled: !allowedActions.includes('increment'),
+//       }, 'Increment'),
+//       h('button', {
+//         onclick (event) {
+//           instance({action: 'decrement', input: 1})
+//         },
+//         disabled: !allowedActions.includes('decrement'),
+//       }, 'Decrement'),
+//     ]),
+//     h('p', [
+//       h('input#inpu', {
+//         placeholder: 'enter here',
+//         style: { position: 'relative' },
+//       }),
+//     ]),
+//     h('p', [
+//       vm.error
+//         ? h('div', [
+//           h('br'),
+//           h('p', vm.error),
+//         ])
+//         : undefined,
+//     ]),
+//   ])
+//   render(view, document.getElementById('root-child'))
+// }
 
-function stateFn (model) {
-  let name
-  let allowedActions = []
+// const model = {
+//   // items: [
+//   //   { id: 0, name: 'foo' },
+//   //   { id: 1, name: 'bar' },
+//   //   { id: 2, name: 'baz' },
+//   // ],
+//   value: undefined,
+//   error: undefined,
+//   parent: undefined,
+// }
 
-  if (model.error === undefined) {
-    name = 'initial'
-    // allowedActions = []
-    allowedActions = Object.keys(actions)
-  }
-  if (model.error === null) {
-    name = 'normal'
-    allowedActions = Object.keys(actions)
-  }
-  if (model.value >= 3) {
-    name = 'max'
-    allowedActions = Object.keys(actions).filter(action => action !== 'increment')
-  }
-  if (model.value <= -3) {
-    name = 'min'
-    allowedActions = Object.keys(actions).filter(action => action !== 'decrement')
-  }
+// function stateFn (model) {
+//   let name
+//   let allowedActions = []
 
-  if (!name) { throw new Error('Invalid state.') }
-  model.state = name
-  return { name, allowedActions }
-}
+//   if (model.error === undefined) {
+//     name = 'initial'
+//     // allowedActions = []
+//     allowedActions = Object.keys(actions)
+//   }
+//   if (model.error === null) {
+//     name = 'normal'
+//     allowedActions = Object.keys(actions)
+//   }
+//   if (model.value >= 3) {
+//     name = 'max'
+//     allowedActions = Object.keys(actions).filter(action => action !== 'increment')
+//   }
+//   if (model.value <= -3) {
+//     name = 'min'
+//     allowedActions = Object.keys(actions).filter(action => action !== 'decrement')
+//   }
 
-const instance = sam()({
-  name: 'child ',
+//   if (!name) { throw new Error('Invalid state.') }
+//   model.state = name
+//   return { name, allowedActions }
+// }
 
-  model,
+// const instance = sam()({
+//   name: 'child ',
 
-  actions,
+//   model,
 
-  present (model, proposal) {
-    model.error = null
+//   actions,
 
-    model.value = proposal.value === undefined
-      ? model.value
-      : proposal.value
+//   present (model, proposal) {
+//     model.error = null
 
-    model.value = proposal.increment === undefined
-      ? model.value
-      : (model.value + proposal.increment) > model.value
-        ? (model.value + proposal.increment) <= 3
-          ? model.value + proposal.increment
-          : model.value
-        : (model.value + proposal.increment) >= -3
-          ? model.value + proposal.increment
-          : model.value
-  },
+//     model.value = proposal.value === undefined
+//       ? model.value
+//       : proposal.value
 
-  stateFn,
+//     model.value = proposal.increment === undefined
+//       ? model.value
+//       : (model.value + proposal.increment) > model.value
+//         ? (model.value + proposal.increment) <= 3
+//           ? model.value + proposal.increment
+//           : model.value
+//         : (model.value + proposal.increment) >= -3
+//           ? model.value + proposal.increment
+//           : model.value
+//   },
 
-  nap (model, state) {
-    stateRepresentation({ vm: model, state })
+//   stateFn,
 
-    const { name } = state
-    if (name === 'initial') {
-      return { action: 'setValue', input: 0 }
-    }
-    if (name === 'max') {
-      parentInstance({ action: 'decrement', input: 1 })
-    }
-    if (name === 'min') {
-      parentInstance({ action: 'setValue', input: 0 })
-    }
-  },
-})
+//   nap (model, state) {
+//     stateRepresentation({ vm: model, state })
+
+//     const { name } = state
+//     if (name === 'initial') {
+//       return { action: 'setValue', input: 0 }
+//     }
+//     if (name === 'max') {
+//       parentInstance({ action: 'decrement', input: 1 })
+//     }
+//     if (name === 'min') {
+//       parentInstance({ action: 'setValue', input: 0 })
+//     }
+//   },
+// })
